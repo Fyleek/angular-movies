5908a9987d5b11f899aeea73bb6eda0ed61f4b7a
Fyleek <odecloquement@gmail.com>
2022-03-03 01:02:14 +0100

    add necessary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'; // TODO : Order import
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/app-movies.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material-module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MovieDataService } from './shared/services/movie-data-service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, MoviesComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [MovieDataService]
 })
 export class AppModule { }
+// TODO : Change favion.ico
